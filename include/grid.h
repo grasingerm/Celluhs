7c58306f329199e3855c627ba42883489cacd493
Matthew Grasinger <grasingerm@gmail.com>
2014-12-02 09:46:08 -0500

    restructuring cell

--- include/grid.h
+++ include/grid.h
@@ -1,20 +1,38 @@
 #ifndef __GRID_H__
 #define __GRID_H__
 
+#include <memory>
+#include <vector>
 #include "types.h"
 
-typedef std::array <double, 2> pos_t
+typedef std::array <double, 2> pos_t;
+
+enum class state
+{
+    ON,
+    OFF,
+    SUSPENDED
+};
 
 struct cell
 {
-    state_t state;
+    state status;
     pos_t position;
+    std::shared_ptr < cell > east_cell;
+    std::shared_ptr < cell > west_cell;
+    std::shared_ptr < cell > north_cell;
+    std::shared_ptr < cell > south_cell;
 };
 
-template < class T, f_uint_t ROWS, f_uint_t COLS >
 class Grid
 {
+public:
+    Grid (const unsigned int rows, const unsigned int cols) : 
 
+    std::vector < std::shared_ptr < cell > > east_bndry;
+    std::vector < std::shared_ptr < cell > > west_bndry;
+    unsigned int n_rows;
+    unsigned int n_cols;
 };
 
 #endif /* __GRID_H__ */
